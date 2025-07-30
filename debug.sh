9e90187d2caef145d78f3770ebea27902c206324
llikian <pokillian64@gmail.com>
2025-07-30 12:49:09 +0200

    Updated CMakeLists

--- debug.sh
+++ debug.sh
@@ -3,4 +3,4 @@
 BUILD_DIR="build-debug"
 
 rm -rf "$BUILD_DIR" bin
-cmake -B "$BUILD_DIR" -DCMAKE_BUILD_TYPE=Debug && cmake --build "$BUILD_DIR" -j
+cmake -B "$BUILD_DIR" -DCMAKE_BUILD_TYPE=Debug -G Ninja && cmake --build "$BUILD_DIR" -j
--- release.sh
+++ release.sh
@@ -3,4 +3,4 @@
 BUILD_DIR="build-release"
 
 rm -rf "$BUILD_DIR" bin
-cmake -B "$BUILD_DIR" -DCMAKE_BUILD_TYPE=Release && cmake --build "$BUILD_DIR" -j
+cmake -B "$BUILD_DIR" -DCMAKE_BUILD_TYPE=Release -G Ninja && cmake --build "$BUILD_DIR" -j
